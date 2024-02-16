671c3d7ad96890309a00d2f167abbfe5d4d28676
Worley03 <andrewworley03@yahoo.com>
2024-02-16 11:56:13 -0600

    Reconnect changes

--- gobbletreact/src/socket.js
+++ gobbletreact/src/socket.js
@@ -1,6 +1,10 @@
 import io from 'socket.io-client';
 
 // This creates a single socket instance.
-const socket = io('https://gobblet-express-backend.onrender.com');
+const socket = io('http://localhost:3000', {
+    reconnection: true,
+    reconnectionAttempts: 5, // Number of attempts
+    reconnectionDelay: 1000, // Delay between attempts in milliseconds
+});
 
 export default socket;
