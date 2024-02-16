5200f578d3333dbabd434c87de94111f8af6ac72
Worley03 <andrewworley03@yahoo.com>
2024-02-16 15:22:44 -0600

    fixes, git is fusttrating me :P

--- gobbletreact/socketserver.js
+++ gobbletreact/socketserver.js
@@ -48,7 +48,6 @@ const io = new socketIo.Server(httpServer, {
 io.on('connection', (socket) => {
     if (socket.recovered) {
         console.log(`User with socket ID ${socket.id} reconnected`);
-
         // recovery was successful: socket.id, socket.rooms and socket.data were restored
     } else {
         console.log(`User with socket ID ${socket.id} connected`);
