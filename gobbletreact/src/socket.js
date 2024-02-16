897dd660defe76e7408190aa9682f251db8286b1
Worley03 <andrewworley03@yahoo.com>
2024-02-16 15:32:09 -0600

    client side for recovery

--- gobbletreact/src/socket.js
+++ gobbletreact/src/socket.js
@@ -7,4 +7,8 @@ const socket = io('https://gobblet-express-backend.onrender.com', {
     reconnectionDelay: 1000, // Delay between attempts in milliseconds
 });
 
+socket.on("connect", () => {
+    console.log("Tried to recover", socket.recovered);
+});
+
 export default socket;
